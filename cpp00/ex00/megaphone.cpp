dbe2cf372a8a07d0f583d67167c329462259c7cd
Hellom Oliveira Mendes <heolivei@c2r1s10.42porto.com>
2024-04-02 20:10:02 +0100

    Atualiza a funcao conforme exigencia de usar o C++98

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <cctype>
 
-int main(int ac, char **av) {
-    if (ac == 1) {
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     } else {
-        for (int i = 1; av[i]; i++) {
-            for (int j = 0; av[i][j]; j++)
-                std::cout << (char) std::toupper(av[i][j]); // Converte para maiúsculas
-            std::cout << " ";
+        for (int i = 1; argv[i] != NULL; i++) {
+            for (int j = 0; argv[i][j] != '\0'; j++) {
+                char c = argv[i][j];
+                c = (c >= 'a' && c <= 'z') ? c - ('a' - 'A') : c; // Converte para maiúsculas se for uma letra minúscula
+                std::cout << c;
+            }
+            std::cout << (argv[i + 1] != NULL ? " " : ""); // Adiciona espaço apenas se houver outra palavra
         }
-		std::cout << std::endl; // Quebra de linha após cada frase
     }
+    std::cout << std::endl; // Quebra de linha após cada frase
     return 0;
 }

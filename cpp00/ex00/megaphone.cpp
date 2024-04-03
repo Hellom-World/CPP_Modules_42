500342f452b9aca5f6d4cde4556ac9fa965c9d7a
Hellom-World <hellom-mendes@hotmail.com>
2024-04-03 20:58:59 +0100

    Tira comentarios do codigo e corrige problema de printar duplo espaços

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -4,15 +4,13 @@ int main(int argc, char *argv[]) {
     if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     } else {
-        for (int i = 1; argv[i] != NULL; i++) {
+        for (int i = 1; i < argc; i++) {
             for (int j = 0; argv[i][j] != '\0'; j++) {
-                char c = argv[i][j];
-                c = (c >= 'a' && c <= 'z') ? c - ('a' - 'A') : c; // Converte para maiúsculas se for uma letra minúscula
-                std::cout << c;
+                std::cout << (char)(argv[i][j] >= 'a' && argv[i][j] <= 'z'
+                    ? argv[i][j] - 32 : argv[i][j]);
             }
-            std::cout << (argv[i + 1] != NULL ? " " : ""); // Adiciona espaço apenas se houver outra palavra
         }
     }
-    std::cout << std::endl; // Quebra de linha após cada frase 
+    std::cout << std::endl;
     return 0;
 }

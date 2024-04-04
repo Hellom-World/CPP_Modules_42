698d1c0fe3e919b708da954c40766f46abef8e0f
Hellom-World <hellom-mendes@hotmail.com>
2024-04-04 20:44:16 +0100

    Cria makefile e coloca o codigo de acordo com a norminette

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    } else {
-        for (int i = 1; i < argc; i++) {
-            for (int j = 0; argv[i][j] != '\0'; j++) {
-                std::cout << (char)(argv[i][j] >= 'a' && argv[i][j] <= 'z'
-                    ? argv[i][j] - 32 : argv[i][j]);
-            }
-        }
-    }
-    std::cout << std::endl;
-    return 0;
+int main(int argc, char *argv[])
+{
+		if (argc == 1)
+				std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		else
+		{
+				for (int i = 1; i < argc; i++)
+						for (int j = 0; argv[i][j] != '\0'; j++)
+								std::cout << (char)(argv[i][j] >= 'a' && argv[i][j] <= 'z'
+										? argv[i][j] - 32 : argv[i][j]);
+		}
+		std::cout << std::endl;
+		return 0;
 }

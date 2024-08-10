83b3c841aa69f28c89498f73d9895f49785ee942
Hellom-World <hellom-mendes@hotmail.com>
2024-08-10 20:28:43 +0100

    finish main ex01

--- cpp05/ex01/src/main.cpp
+++ cpp05/ex01/src/main.cpp
@@ -55,5 +55,70 @@ int main(void)
     //std::cout << "------------------------------------------" << std::endl;
     //testInvalidBureucratGradeToForm();
     //std::cout << "------------------------------------------" << std::endl;
+
+    //invalid createform high grade
+    try
+    {
+        Form form("plan", 0, 1);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    //invalid create form low grade
+    try
+    {
+        Form form("plan", 151, 1);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    //invalid creat bureaucrat high grade
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 0);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    //invalid create bureaucrat low grade
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 151);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    //invalid sign form high grade
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 1);
+        Form form("plan", 0, 1);
+        form.beSigned(bureaucrat);
+        form.beSigned(bureaucrat);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    //invalid sign form low grade
+    try
+    {
+        Bureaucrat bureaucrat("Bureaucrat", 150);
+        Form form("plan", 1, 1);
+        form.beSigned(bureaucrat);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }

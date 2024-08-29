b1ea39d364f9b3d49521a77e2215fc91d99cd6dc
Hellom-World <hellom-mendes@hotmail.com>
2024-08-29 13:26:12 +0100

    finish ex02 cpp07

--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -2,12 +2,16 @@
 
 int    main()
 {
+    //Testa construtor sem parametro
+    Array<int> empty;
+    std::cout << "Array<int> empty;" << std::endl;
+    std::cout << "------------Create Array a--------------------" << std::endl;
     Array<int> a(5);
     for (unsigned int i = 0; i < a.size(); i++)
         std::cout << a[i] << std::endl;
+    std::cout << "<Add values to Array a>" << std::endl;
     for (unsigned int i = 0; i < a.size(); i++)
         a[i] = i;
-    std::cout << "----------------" << std::endl;
     for (unsigned int i = 0; i < a.size(); i++)
         std::cout << a[i] << std::endl;
 
@@ -25,6 +29,51 @@ int    main()
     std::cout << "------print Array a----------" << std::endl;
     for (unsigned int i = 0; i < a.size(); i++)
         std::cout << a[i] << std::endl;
+
+
+    //Testa exceção
+    std::cout << "-------Exception test--------" << std::endl;
+    try
+    {
+        std::cout << "<TRY Add a[-1] = 0;>" << std::endl;
+        a[-1] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "ERR0R: out of range" << std::endl;
+    }
+    std::cout << "--------------------------------" << std::endl;
+    try
+    {
+        std::cout << "<TRY Add a[5] = 0;>" << std::endl;
+        a[5] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "ERR0R: out of range" << std::endl;
+    }
+    std::cout << "--------------------------------" << std::endl;
+
+    //Testa array.size()
+    std::cout << "Array<int> c;" << std::endl;
+    Array<int> c;
+    std::cout << "c.size() = " << c.size() << std::endl;
+    std::cout << "Array<int> d(10);" << std::endl;
+    Array<int> d(10);
+    for (unsigned int i = 0; i < d.size(); i++)
+    {
+        d[i] = i;
+        std::cout << "d[" << i << "] = ";
+        std::cout << d[i] << std::endl;
+    }
+    std::cout << "d.size() = " << d.size() << std::endl;
+    for (unsigned int i = 0; i < d.size(); i++)
+    {
+        std::cout << "d[" << i << "] = ";
+        std::cout << d[i] << std::endl;
+    }
+
+
 
     return 0;
 }
